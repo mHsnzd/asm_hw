0cc086c7ed1f1a78a7a506566139e302ed903b13
marjanh <marjanhassanzadehjahromi@gmail.com>
2021-05-25 19:04:20 +0430

    ImprovedSelectCase
    Add ImprovedSelectCase procedure to perform the same switch statement without needing the values of the case and x to match.

--- selectCase.asm
+++ selectCase.asm
@@ -1,7 +1,11 @@
 ; select case - 64 bit - MASM                    (selectCase.asm)
 
-; Program Description: Assembly code that does the same job as the 
-;                      C switch statement below: 
+; Program Description: SelectCase is an assembly procedure that does the exact same job as the 
+;                      C switch statement below. ImprovedSelectCase does the same thing, however, 
+;                      it also works if the cases do not correspond to the values given to x. It  
+;                      does so by finding the corresponding element in ValueTable based on the case's 
+;                      position in CaseTable.
+;
 ;					   switch(i) {
 ;						    case 1:
 ;						    {
@@ -51,9 +55,12 @@ main proc
 	                                    
 	mov i, 5                                     ; i = 5
 	call SelectCase                              ; rdx = 1 (error: 5 is not a case)
+	call ImprovedSelectCase                      ; rdx = 1 (error: 5 is not a case)
+
 
 	mov i, 1                                     ; i = 1
 	call SelectCase                              ; x = 1 rdx = 0
+	call ImprovedSelectCase                      ; x = 1 rdx = 0
 
 	call  ExitProcess
 main endp
@@ -86,4 +93,33 @@ SelectCase PROC uses rax rsi rbx rdi                ; MASM directive to preserve
 done:	ret
 SelectCase ENDP
 
+
+;---------------------------------------------------------
+ImprovedSelectCase PROC uses rdi rcx rax rsi        ; MASM directive to preserve these registers
+; Receives: nothing
+; Returns: rdx = 0 in case of success and 1 in case of failure. Sets x to the correct value.
+;          Changes the direction and status flags.
+; Requires: nothing
+;---------------------------------------------------------
+	mov  rdx, 1                                     ; set rdx to one in case of error
+
+	movsx rax, i
+	mov rdi, offset CaseTable                       ; rdi points to CaseTable's first element
+	mov rsi, offset ValueTable                      ; rsi points to ValueTable's first element
+	mov rcx, TableCount                             ; rcx = number of cases
+	cld                                             ; clear direction flag so scasq will increment rdi
+l1:	
+	scasb                                           ; compare i (rax) to the cases (rdi) and increment rdi
+	loopnz l1                                       ; loop until the case is found or the cases are finished
+
+	jnz done                                        ; terminates the procedure if case isn't found
+
+	neg rcx                                         ; converting rcx to the right index
+	add rcx, TableCount - 1                         
+
+	mov al, [rsi + rcx * type ValueTable]           ; the corresponding value in ValueTable      
+	mov x, al                                       ; setting x to the correct value
+	xor rdx, rdx                                    ; clears rdx for success                            
+done:	ret
+ImprovedSelectCase ENDP
 end
